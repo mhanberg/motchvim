6918a81cf377a6a1d4b7221e86a5fd537e4dfaaf
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-07-16 09:58:31 -0400

    feat: which-key.nvim
    fix: fix some keybinds to add descriptions

--- lua/motchvim/autocmds.lua
+++ lua/motchvim/autocmds.lua
@@ -19,7 +19,7 @@ autocmd("LspAttach", {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     assert(client, "LSP is yet to be found")
     local map_opts = function(override)
-      vim.tbl_extend("force", { buffer = bufnr, silent = true }, override or {})
+      return vim.tbl_extend("force", { buffer = bufnr, silent = true }, override or {})
     end
 
     autocmd("BufWritePre", {
@@ -39,14 +39,14 @@ autocmd("LspAttach", {
       end
     end
 
-    vim.keymap.set("n", "df", function()
+    vim.keymap.set("n", "<leader>cf", function()
       require("conform").format { async = false, lsp_fallback = true, id = client.id }
     end, map_opts { desc = "Format file" })
-    vim.keymap.set("n", "gd", vim.diagnostic.open_float, map_opts { desc = "Open diagnostic window" })
+    vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, map_opts { desc = "Open diagnostic window" })
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, map_opts { desc = "Prev. diagnostic" })
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, map_opts { desc = "Next diagnostic" })
 
-    vim.keymap.set("n", "dt", vim.lsp.buf.definition, map_opts { desc = "Go to definition" })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts { desc = "Go to definition" })
     vim.keymap.set("n", "K", vim.lsp.buf.hover, map_opts { desc = "Show hover documentation" })
     vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, map_opts { desc = "Go to type definition" })
     vim.keymap.set("n", "gr", fzf("lsp_references"), map_opts { desc = "Find references" })
@@ -56,17 +56,17 @@ autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>ca", fzf("lsp_code_actions", {}), map_opts { desc = "Code Actions" })
     vim.keymap.set(
       "n",
-      "<leader>dd",
+      "<leader>sd",
       fzf("lsp_document_diagnostics"),
       map_opts { desc = "Buffer diagnostics" }
     )
     vim.keymap.set(
       "n",
-      "<leader>da",
+      "<leader>sD",
       fzf("lsp_workspace_diagnostics"),
       map_opts { desc = "Workspace diagnostics" }
     )
-    vim.keymap.set("n", "<space>r", vim.lsp.codelens.run, map_opts { desc = "Run codelens" })
+    vim.keymap.set("n", "<leader>cc", vim.lsp.codelens.run, map_opts { desc = "Run codelens" })
 
     vim.cmd([[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
     vim.cmd([[smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
@@ -76,14 +76,14 @@ autocmd("LspAttach", {
         "n",
         "<space>tp",
         ":Elixir nextls to-pipe<cr>",
-        { desc = "Convert nested syntax to pipe syntax" }
+        map_opts { desc = "Convert nested syntax to pipe syntax" }
       )
 
       vim.keymap.set(
         "n",
         "<space>fp",
         ":Elixir nextls from-pipe<cr>",
-        { desc = "Convert pipe syntax to nested syntax" }
+        map_opts { desc = "Convert pipe syntax to nested syntax" }
       )
     end
 
@@ -168,14 +168,6 @@ autocmd("FileType", {
   end,
 })
 
-autocmd("FileType", {
-  group = random,
-  pattern = "fzf",
-  callback = function()
-    vim.cmd.setlocal("winhighlight+=Normal:Normal")
-    vim.keymap.set("t", "<esc>", "<C-c>", { buffer = 0, desc = "Exit fzf" })
-  end,
-})
 autocmd(
   { "BufRead", "BufNewFile" },
   { group = random, pattern = "*.livemd", command = "set filetype=markdown" }
