4f05dc6e96aee9f7ea30767c74849dcec6b1472a
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-02-26 12:22:24 -0500

    feat(expert): easy expert override

--- lsp/expert.lua
+++ lsp/expert.lua
@@ -1,7 +1,9 @@
+local cmd = vim.env.EXPERT_PATH or "expert"
 return {
-  cmd = { "expert", "--stdio" },
+  cmd = { cmd, "--stdio" },
   filetypes = { "elixir" },
   root_markers = {
+    "mix.lock",
     "mix.exs",
   },
 }
